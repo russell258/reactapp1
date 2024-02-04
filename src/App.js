cf18a768afb8f144905a673a15e19a6f569b8b42
Russell258 <duckie258@gmail.com>
2024-02-04 11:56:49 +0800

    import export, inline css formats

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './Custom.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //this is jsx, not HTML. allows you to write html elements in
+    // javascript and place in DOM without any createElement()
+
+    //css background-color -> backgroundColor, border-color -> borderColor
+    // value need to add a signle quote
+    // semi colon --> comma
+
+    return (
+    <div className="outerdiv">
+      <div className="innerdiv">This is a green box</div>
+      <div style={{padding:'70px 0',backgroundColor:'blue'}}>
+        This is inline css in blue
+      </div>
     </div>
-  );
+    
+    )
 }
 
 export default App;
